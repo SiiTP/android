f874e5b17e4c388f0bc5fcbc440cda997b5e3427
Иван <reg.lo@yandex.ru>
2016-03-30 17:41:01 +0400

    began work with reminder fragment

---
+++ app/src/main/java/com/dbtest/ivan/app/fragment/ReminderListFragment.java
@@ -0,0 +1,10 @@
+package com.dbtest.ivan.app.fragment;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by Ivan on 30.03.2016.
+ */
+public class ReminderListFragment extends ListFragment {
+
+}
