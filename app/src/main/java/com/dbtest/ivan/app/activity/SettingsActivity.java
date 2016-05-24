860b290fbfd6c0a0b729293574dbca612f5dfa3e
ivanbmstu <Purat33@gmail.com>
2016-05-24 21:30:05 +0300

    settings delete oncreate

--- app/src/main/java/com/dbtest/ivan/app/activity/SettingsActivity.java
+++ app/src/main/java/com/dbtest/ivan/app/activity/SettingsActivity.java
@@ -1,6 +1,5 @@
 package com.dbtest.ivan.app.activity;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 import com.dbtest.ivan.app.R;
@@ -19,10 +18,5 @@ public class SettingsActivity extends AbstractToolbarActivity {
     @Override
     public Integer getMenuPosition() {
         return MENU_POSITION;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 }
